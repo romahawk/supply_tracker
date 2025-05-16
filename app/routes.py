28aa80fdf67b86d6c951c7fbbc5b2034d8b1e490
romahawk <romazuryk@gmail.com>
2025-05-16 14:11:02 +0200

    fix: add missing login_user() call to enable user session
    Fixed login route by adding missing login_user() call
    
    Ensures user session is created upon successful login
    
    Prevents redirect loop and "Please log in" message
    
    Verified against hashed credentials (RM / Rm1001)

--- app/routes.py
+++ app/routes.py
@@ -41,6 +41,7 @@ def login():
 
         if user and check_password_hash(user.password, password):
             remember = True if request.form.get('remember') == 'on' else False
+            login_user(user, remember=remember)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('main.dashboard'))
         else:
